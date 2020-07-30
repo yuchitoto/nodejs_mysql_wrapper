9c4617411b8ae7304de592135b6c22d35f01dc04
Yu Chi To <yuchitoto@gmail.com>
2020-07-30 15:42:49 +0800

    fix bug

--- MySQLConnection.js
+++ MySQLConnection.js
@@ -6,9 +6,9 @@
 * VERSION: 1.0.1.1 (8-7-2020)
 */
 
-const conn = require('./src/mysqlConnection.js');
-const db = require('./src/mysqlDB.js');
-const tb = require('./src/MySQLTables.js');
+const conn = require(__dirname + '/src/mysqlConnection.js');
+const db = require(__dirname + '/src/mysqlDB.js');
+const tb = require(__dirname + '/src/mysqlTables.js');
 
 module.exports = {
   MySQLConnection:conn.MySQLConnection,
